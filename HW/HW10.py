9c067ee3516cddc1c795c517c98118396265403a
VladimirK <vladimir1kuzmin@gmail.com>
2023-03-21 11:42:14 +0700

    upd HW

--- HW/HW10.py
+++ HW/HW10.py
@@ -7,15 +7,31 @@
 # 5 -> 1 0 1 1 0
 # 2
 
-n = int(input())
-count_zero = 0
-count_one = 0
+# n = int(input())
+# count_zero = 0
+# count_one = 0
+# for i in range(n):
+#     x = int(input()) 
+#     if x == 0:
+#         count_zero += 1 
+#     else:
+#         count_one += 1
+# if count_one > count_zero:
+#     print(count_zero)
+# else: print(count_one)
+
+n = int(input('Введите количество монеток: '))
+count1 = 0
+count2 = 0
+print('Вводите 1 или 0: ')
 for i in range(n):
-    x = int(input()) 
-    if x == 0:
-        count_zero += 1 
-    else:
-        count_one += 1
-if count_one > count_zero:
-    print(count_zero)
-else: print(count_one)
+    temp = int(input())
+    if temp == 0:
+        count1 += 1
+    if temp == 1:
+        count2 += 1
+if count1 > count2:
+    res = count2
+else:
+    res = count1
+print(f'Результат: {res}')
